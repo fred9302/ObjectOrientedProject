96f4d41b25b431eb745320d656c8124a8e452e00
Frederik <frederikhoue@gmail.com>
2023-12-02 20:55:37 +0000

    Created class for devices and made a get and a set function

---
+++ devices.py
@@ -0,0 +1,11 @@
+class devices:
+    def __init__(self):
+        self.data = None
+            
+        def get_data(self):
+            
+            return self.data
+            
+        def set_data(self, new_data):
+            self.data = new_data
+    
