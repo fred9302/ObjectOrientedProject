42e9140bf137808fcdcccb6645c8066d51fd35de
Frederik <frederikhoue@gmail.com>
2023-12-04 21:35:25 +0000

    Moved unused functions to temporary file called examples

--- code/main.py
+++ code/main.py
@@ -2,17 +2,17 @@ import simulation as netSim
 
 
 if __name__ == '__main__':
+    sim = netSim.simulation();
+    
     x = int(input("Enter number of columns: "))
     y = int(input("Enter number of rows: "))
-    debug = str(input("Verbose ? y/n: "))
+    debug = str(input("Verbose? y/n: "))
     if debug == 'y':
-        debug = True
+        sim.set_verbose(True)
     elif debug == 'n':
-        debug = False
+        sim.set_verbose(False)
     else:
         print("Invalid input")
         exit()
-        
     
-    sim = netSim.simulation();
-    sim.start_simulation(x, y, debug);
+    sim.start_simulation(x, y);
--- code/simulation.py
+++ code/simulation.py
@@ -3,17 +3,10 @@ from configparser import ConfigParser
 
 class simulation:
     def __init__(self):
-        self.data = None
         self.dBconn = None
         self.clock = None
         self.grid = None
         self.verbose = None
-            
-    def get_data(self):
-        return self.data
-            
-    def set_data(self, new_data):
-        self.data = new_data
         
     def add_time(self, time = 0, print_time = False):
         """
@@ -73,75 +66,3 @@ class simulation:
     def start_simulation(self, columns = 0, rows = 0):
         print('Starting simulation...')
         self.__create_grid(columns, rows, self.verbose)
-        
-        
-    def db_config(self, filename='/home/frederik/GitHub/ObjectOrientedProject/code/database.ini', section='postgresql'):
-        """
-        Reads the database configuration from the specified INI file.
-            
-        Args:
-            filename (str, optional): The name of the INI file. Defaults to 'database.ini'.
-            section (str, optional): The section name in the INI file. Defaults to 'postgresql'.
-            
-        Returns:
-            dict: A dictionary containing the database configuration parameters.
-            
-        Raises:
-            Exception: If the specified section is not found in the INI file.
-        """
-            
-        # create a parser
-        parser = ConfigParser()
-            
-        # read config file
-        parser.read(filename)
-
-        # get section, default to postgresql
-        db = {}
-        if parser.has_section(section):
-            params = parser.items(section)
-            for param in params:
-                db[param[0]] = param[1]
-        else:
-            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-        return db
-        
-    def connect(self):
-        """
-        Connects to the PostgreSQL database using the provided connection parameters and print the PostgreSQL database server version.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-        """
-            
-        try:
-            # read connection parameters
-            params = self.db_config()
-            
-            # connect to the PostgreSQL server
-            print('Connecting to the PostgreSQL database...')
-            self.dbConn = psycopg2.connect(**params)
-            
-            # create a cursor
-            cur = self.dbConn.cursor()
-            
-            # execute a statement
-            print('PostgreSQL database version:')
-            cur.execute('SELECT version()')
-            
-            # display the PostgreSQL database server version
-            db_version = cur.fetchone()
-            print(db_version)
-            
-            # close the communication with the PostgreSQL
-            cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            if self.dbConn is not None:
-                self.dbConn.close()
-                print('Database connection closed.')
